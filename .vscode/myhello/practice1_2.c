b15b8b9674d920b212e2be8323d27a7a7cabd393
liuzhenxiao_lzx <sdhzlzx@qq.com>
2020-09-06 10:54:06 +0800

    测试

--- .vscode/myhello/practice1_2.c
+++ .vscode/myhello/practice1_2.c
@@ -1,2 +1,11 @@
 #include<math.h>
 #include<stdio.h>
+
+void main() 
+{
+    double x, s;
+    printf("input number: \n");
+    scanf("%lf, &x");
+    s=sin (x);
+    printf("sine of %lf is %lf\n", x, s);
+}
